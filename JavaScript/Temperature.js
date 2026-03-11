1323a93b8aa672ed0163096ec63a2aaadb69f29f
himaindla576 <himaindla576@gmail.com>
2026-03-11 20:22:37 +0530

    fix(temperature):resolving mistakes

--- JavaScript/Temperature.js
+++ JavaScript/Temperature.js
@@ -1,6 +1,7 @@
 function onClickConverterFToC(){
+    debugger;
     fTemperature=parseFloat(document.getElementById("txttemperature").value)
-    fTemperature = cTemperature * 1.8 + 32
+    cTemperature = fTemperature * 1.8 + 32
       document.getElementById("tempResult").innerHTML = "result:" + fTemperature + "c"
 
 }
