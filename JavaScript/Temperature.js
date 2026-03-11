f47ca24c097e1c1fa716844140c654359fbd9f4f
himaindla576 <himaindla576@gmail.com>
2026-03-11 20:16:54 +0530

    adding script

--- JavaScript/Temperature.js
+++ JavaScript/Temperature.js
@@ -1,1 +1,6 @@
-function onclickTemperature()
+function onClickConverterFToC(){
+    fTemperature=parseFloat(document.getElementById("txttemperature").value)
+    fTemperature = cTemperature * 1.8 + 32
+      document.getElementById("tempResult").innerHTML = "result:" + fTemperature + "c"
+
+}
