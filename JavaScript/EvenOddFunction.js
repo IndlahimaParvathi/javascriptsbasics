2a7309ab367049537c16af2e85ba93c28db1fe25
himaindla576 <himaindla576@gmail.com>
2026-03-12 20:59:51 +0530

    buliding js script

---
+++ JavaScript/EvenOddFunction.js
@@ -0,0 +1,28 @@
+function onClickEvenOdd(){
+    debugger;
+ EvenOdd=parseFloat(document.getElementById("txtEvenOdd").value)
+ let number=5
+ let remainder=(number%2)
+ if(remainder==0){
+    
+ }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
