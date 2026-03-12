1db458f5b813fc6426d8e67189c201cf92ec87e8
himaindla576 <himaindla576@gmail.com>
2026-03-12 21:03:06 +0530

    css

--- JavaScript/Temperature.js
+++ JavaScript/Temperature.js
@@ -2,6 +2,6 @@ function onClickConverterFToC(){
     debugger;
     fTemperature=parseFloat(document.getElementById("txttemperature").value)
     cTemperature = fTemperature * 1.8 + 32
-      document.getElementById("tempResult").innerHTML = "answer;" + cTemperature + "c"
+      document.getElementById("tempResult").innerHTML = "result;" + fTemperature + "f"
 
 }
