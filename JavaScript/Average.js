b96badab997cc69e6ee14bd550fce764e8793dca
himaindla576 <himaindla576@gmail.com>
2026-03-14 15:29:10 +0530

    feat(average):writing js code

---
+++ JavaScript/Average.js
@@ -0,0 +1,11 @@
+function onClickAverage(){
+debugger;
+number1=parseFloat(document.getElementById("txtNum1").value)
+number2=parseFloat(document.getElementById("txtNum2").value)
+number3=parseFloat(document.getElementById("txtNum3").value)
+number4=parseFloat(document.getElementById("txtNum4").value)
+number5=parseFloat(document.getElementById("txtNum5").value)
+number6=parseFloat(document.getElementById("txtNum6").value)
+Average=number1+number2+number3+number4+number5+number6/6
+document.getElementById("pResult").innerHTML="result:"+average ;
+}
