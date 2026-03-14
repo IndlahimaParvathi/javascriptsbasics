7bb460c3a21fb36bc7dff52d181e2e579faade66
himaindla576 <himaindla576@gmail.com>
2026-03-14 13:21:00 +0530

    feat(Evenoddfunction):writing the code in another way

--- JavaScript/EvenOddFunction.js
+++ JavaScript/EvenOddFunction.js
@@ -1,16 +1,32 @@
+// function onClickEvenOdd(){
+//     debugger;
+//  EvenOdd=parseFloat(document.getElementById("txtEvenOdd").value)
+
+//  remainder=(EvenOdd%2)
+//  if(remainder==0){
+//     document.getElementById("pResult").innerHTML="Even"
+//  }
+// if(remainder!=0){
+//     document.getElementById("pResult").innerHTML="Odd"
+// }
+// }
+
+
 function onClickEvenOdd(){
     debugger;
- EvenOdd=parseFloat(document.getElementById("txtEvenOdd").value)
 
- let remainder=(number%2)
- if(remainder==0){
-    document.getElementById("pResult").innerHTML="Even"
+ var  number= parseInt(document.getElementById("txtEvenOdd").value);
+ if (number%2==0) {
+    document.getElementById("pResult").innerHTML= number+ " is Even Number" ;
+
  }
-if(remainder!=0){
-    document.getElementById("pResult").innerHTML="Odd"
-}
-}
+  else;
 
+{
+  document.getElementById("pResult").innerHTML= number+ " is Odd Number" ;
+
+ }
+}
 
 
 
