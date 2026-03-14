35511f3fe573f33254ade1364a0c6c9acf11bbdc
himaindla576 <himaindla576@gmail.com>
2026-03-14 12:50:41 +0530

    feat(EvenOddFunction):writing js code

--- JavaScript/EvenOddFunction.js
+++ JavaScript/EvenOddFunction.js
@@ -1,12 +1,14 @@
 function onClickEvenOdd(){
     debugger;
  EvenOdd=parseFloat(document.getElementById("txtEvenOdd").value)
- let number=5
+
  let remainder=(number%2)
  if(remainder==0){
-    
+    document.getElementById("pResult").innerHTML="Even"
  }
-
+if(remainder!=0){
+    document.getElementById("pResult").innerHTML="Odd"
+}
 }
 
 
